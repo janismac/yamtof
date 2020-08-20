e6881a3c74253a85dd98b8dc73954302aef203b9
Janis <>
2020-08-20 13:28:31 +0200

    setup.py add readme

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name = 'yamtof',
     version = '0.1',
     license = 'apache-2.0',
     packages = ['yamtof', 'yamtof.examples'],
     description = 'Yamtof provides a simple and convenient interface for writing multi-phase optimal control problems.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'Janis Maczijewski',
     author_email = 'Janis.Maczijewski@rwth-aachen.de',
     url = 'https://github.com/janismac/yamtof',
